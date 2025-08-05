932778a3132955227444de416b9b76569f30d113
Aditi Patil <patiladiti2206@gmail.com>
2025-08-05 12:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (85.11%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+ void backtrack(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>&       result) {
+    result.push_back(current);  
+
+    for (int i = start; i < nums.size(); i++) {
+        // Skip duplicates at the same level
+        if (i > start && nums[i] == nums[i - 1]){
+            continue;
+        }
+        current.push_back(nums[i]);               
+        backtrack(i + 1, nums, current, result);  
+        current.pop_back();                      
+    }
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> result;
+    vector<int> current;
+
+    sort(nums.begin(), nums.end());  // duplicate check
+    backtrack(0, nums, current, result);
+    return result;
+    }
+};
