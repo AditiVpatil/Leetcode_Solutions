26d42ef8a3b3bc5ef1e7c23e7539123b9d5fbbaa
Aditi Patil <patiladiti2206@gmail.com>
2025-08-14 20:33:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (52.45%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        vector<string> triplets;
+        string maxTriplet = "";
 
-        //  Traverse and collect triplets
-        for (int i = 0; i + 2 < (int)num.size(); i++) {
+        for (int i = 0; i + 2 <num.size(); i++) {
             if (num[i] == num[i+1] && num[i] == num[i+2]) {
-                triplets.push_back(num.substr(i, 3));
+                string triplet = num.substr(i, 3);
+                if (triplet > maxTriplet) {
+                    maxTriplet = triplet;
+                }
             }
         }
-
-        //  If no triplet found, return empty
-        if (triplets.empty()) return "";
-
-        // Find the maximum triplet
-        string maxTriplet = triplets[0];
-        for (string &t : triplets) {
-            if (t > maxTriplet) {
-                maxTriplet = t;
-            }
-        }
-
         return maxTriplet;
     }
 };
