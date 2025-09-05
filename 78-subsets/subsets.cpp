3f3033415a22ba65d3e4a4f93d8565fe449a17ce
Aditi Patil <patiladiti2206@gmail.com>
2025-09-05 13:51:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (42.16%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int total = 1 << n;  // 2^n subsets
         vector<vector<int>> res;
-        vector<int> curr;
-        dfs(0, nums, curr, res);
-        return res;
-    }
 
-    void dfs(int indx, vector<int>& nums, vector<int>& curr, vector<vector<int>>& res) {
-        if (indx == nums.size()) {
-            res.push_back(curr);
-            return;
+        for (int i = 0; i < total; i++) {
+            vector<int> subset;
+            for (int j = 0; j < n; j++) {
+                if (i & (1 << j)) { // check if jth bit is set
+                    subset.push_back(nums[j]);
+                }
+            }
+            res.push_back(subset);
         }
-
-        // Include nums[indx]
-        curr.push_back(nums[indx]);
-        dfs(indx + 1, nums, curr, res);
-
-        // Exclude nums[indx]
-        curr.pop_back();
-        dfs(indx + 1, nums, curr, res);
+        return res;
     }
 };
