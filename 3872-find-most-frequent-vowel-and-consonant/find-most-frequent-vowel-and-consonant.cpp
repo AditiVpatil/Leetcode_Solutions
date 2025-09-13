882c062c97cb6187061fcd15a1646692f1b2d3e7
Aditi Patil <patiladiti2206@gmail.com>
2025-09-13 10:45:44 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (30.82%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        unordered_map<char,int> fv, fc;
-        string vowels = "aeiouAEIOU";
-        unordered_set<char> vowelSet(vowels.begin(), vowels.end());
-
-        // Count frequencies
-        for(char c : s) {
-            if(isalpha(c)) {
-                if(vowelSet.count(c)) fv[c]++;
-                else fc[c]++;
+        int count_v=0;
+        int count_c=0;
+        unordered_map<char,int>mp;
+         for(int i=0;i<s.size();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+                mp[s[i]]++;
+                count_v=max(count_v,mp[s[i]]);
+            }
+            else{
+            mp[s[i]]++;
+                count_c=max(count_c,mp[s[i]]);
             }
         }
-
-        // Get max vowel freq
-        int maxV = 0;
-        if(!fv.empty()) {
-            maxV = max_element(fv.begin(), fv.end(),
-                        [](auto &a, auto &b){ return a.second < b.second; })->second;
-        }
-
-        // Get max consonant freq
-        int maxC = 0;
-        if(!fc.empty()) {
-            maxC = max_element(fc.begin(), fc.end(),
-                        [](auto &a, auto &b){ return a.second < b.second; })->second;
-        }
-
-        return maxV + maxC;
+        return count_v+count_c;
     }
 };
