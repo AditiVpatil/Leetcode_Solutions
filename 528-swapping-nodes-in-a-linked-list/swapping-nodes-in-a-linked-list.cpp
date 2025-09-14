d19a09d2ce92b126ff62fde71488285572b95541
Aditi Patil <patiladiti2206@gmail.com>
2025-09-14 23:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 185.1 MB (95.01%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* temp = head;
-        int n = 0;
-
-        
-        while (temp != NULL) {
-            n++;
-            temp = temp->next;
-        }
-
-      
-        ListNode* f = head;
+        ListNode* first = head;
         for (int i = 1; i < k; i++) {
-            f = f->next;
+            first = first->next;
         }
 
-        
-        ListNode* s = head;
-        for (int i = 1; i < n - k + 1; i++) {
-            s = s->next;
+        ListNode* second = head;
+        ListNode* fast = first;
+
+        while (fast->next != NULL) {
+            fast = fast->next;
+            second = second->next;
         }
 
-        
-        swap(f->val, s->val);
+        swap(first->val, second->val);
 
         return head;
     }
