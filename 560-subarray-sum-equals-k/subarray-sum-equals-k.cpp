8449cb7cd42fdbcffc9bb31d58a8c4050de7bfe9
Aditi Patil <patiladiti2206@gmail.com>
2025-10-14 22:26:40 +0530

    Time: 55 ms (45.14%) | Memory: 51.1 MB (16.34%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-    unordered_map<int,int> mp;
-    mp[0] = 1;  //starting with 0th index
-    int sum = 0, count = 0;
+    unordered_map<int,int> mp;  // stores prefixSum -> frequency
+    mp[0] = 1;  // base case: one way to get sum 0 (empty subarray)
 
-    for (int x : nums) {
-        sum += x;
-        if (mp.count(sum - k)){
-            count += mp[sum - k];
-        }    
-        mp[sum]++;
+    int prefixsum = 0;  // running sum
+    int cnt = 0;        // count of valid subarrays
+
+    for(int i = 0; i < nums.size(); i++) {
+        prefixsum += nums[i];  
+
+        int rem = prefixsum - k;  // what we want to find in the past
+
+        cnt += mp[rem];  // add how many times 'rem' has appeared so far
+
+        mp[prefixsum]++; // store current prefix sum for future use
     }
-    return count;
-  }
+
+    return cnt;
+}
 
 };
