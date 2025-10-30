5318eb79fd54c8b8221fc7a1c01fbfd282b52fea
Aditi Patil <patiladiti2206@gmail.com>
2025-10-30 21:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (60.49%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -3,7 +3,7 @@ public:
     int smallestNumber(int n) {
         int ans = 1;
         while (ans < n) {
-            ans = (ans << 1) | 1;
+            ans = (ans * 2)+1;
         }
         return ans;
     }
