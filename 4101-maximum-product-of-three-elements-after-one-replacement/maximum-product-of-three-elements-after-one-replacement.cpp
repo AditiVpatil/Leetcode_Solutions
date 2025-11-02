2a48fe52c46fa96addd79406e4c4d686f260e40f
Aditi Patil <patiladiti2206@gmail.com>
2025-11-02 22:07:41 +0530

    Time: 32 ms (64.29%) | Memory: 124.4 MB (64.29%) - LeetSync

---
+++ 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long maxProduct(vector<int>& nums) {
+        int n=nums.size() ;
+        for(auto &x :nums) x=abs(x) ;
+        sort(nums.begin(),nums.end()) ;
+        return (long long) nums[n-1]*nums[n-2]*100000 ;
+    }
+};
