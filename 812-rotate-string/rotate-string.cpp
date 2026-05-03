e3aa929b66c88bb9d63f3cf5d639e3f22c251556
Aditi Patil <patiladiti2206@gmail.com>
2026-05-03 20:09:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (36.38%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -4,10 +4,10 @@ public:
         if (s.length() != goal.length()) return false;
 
         for (int i = 0; i < s.length(); i++) {
-            // Rotate s by 1 (left rotation)
+         
             rotate(s.begin(), s.begin() + 1, s.end());
                if (s == goal) return true;
         }
-        return s == goal; // if  they are already equal
+        return s == goal; 
     }
 };
