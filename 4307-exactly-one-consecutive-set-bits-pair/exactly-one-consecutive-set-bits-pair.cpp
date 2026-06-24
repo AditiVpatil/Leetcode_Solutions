4d4ef296902e4c98687491cf4e0ba75d74ddeaec
Aditi Patil <patiladiti2206@gmail.com>
2026-06-24 11:04:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (14.81%) - LeetSync

--- 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.cpp
+++ 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool consecutiveSetBits(int n) {
-         int count = 0;
-
-    while (n > 0) {
-        if ((n & 3) == 3) { 
-            count++;
+        string b = bitset<64>(n).to_string();
+        b.erase(0, b.find('1')); 
+        int count = 0;
+        for (int i = 0; i + 1 < b.size(); i++) {
+            if (b[i] == '1' && b[i+1] == '1') {
+                count++;     }
         }
-        n >>= 1;
-    }
-
-    return count == 1;
+        return count == 1;
     }
 };
